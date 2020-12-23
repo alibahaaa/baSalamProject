1f7ba79b85a56e753dc8419e9e7a3b609c966c36
Ali Baha <alibahaabadii@gmail.com>
2020-12-23 07:59:34 +0330

    Ba Salam Test Project changes

--- app/src/main/java/com/basalam/test/ui/MainActivity.kt
+++ app/src/main/java/com/basalam/test/ui/MainActivity.kt
@@ -6,19 +6,6 @@ import com.basalam.test.R
 
 class MainActivity : AppCompatActivity() {
 
-    /*
-    From ALI BAHAABADI To Mr. NASIRI
-    This project developed for Ba Salam.
-    In this project I use
-    Retrofit,
-    RecyclerView,
-    Picasso,
-    Kotlin Coroutines
-    and MVVM
-    Im familiar with Hilt and Room but not expert, so i don't use them !
-    Also I'm expert in Volley but I found retrofit better , so why Volley ?
-     */
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
